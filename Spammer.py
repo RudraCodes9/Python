c476d20e492227284f6ab855aec0cb704ae16c5b
Rudra L <83490859+RudraCodes9@users.noreply.github.com>
2022-07-12 21:30:35 +0530

    Add files via upload

---
+++ Spammer.py
@@ -0,0 +1,9 @@
+#THIS A SPAMMER CODE, WHICH SPAMS. THE SETUP IS MANUAL.#
+
+import pyautogui , time
+time.sleep(5) #Time Before Launch#
+
+for i in range(10): #Times you want to spam#
+    pyautogui.typewrite("Text")
+    pyautogui.press("enter")
+    time.sleep(.1)
